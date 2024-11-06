ef4c5c74bc8d790726e0c4554afa8fb5e3bcea40
LAPTOP-3QO177P3\shahp <shahprenik@gmail.com>
2024-11-06 09:04:06 +0530

    Code optimization disk size with LTO

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "psgaming-PirateRush_WebDemo1-0.1";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/fd42abddfb4cd6d9eea9609846e3cb95.framework.js",
-    "Build/9eb7a4cb65f33823ef2c32114c27f8b6.data",
-    "Build/b96225bbda6fa332878e68f279d0d416.wasm",
+    "Build/08be164d17ff39ab3c365775127f0ed1.framework.js",
+    "Build/42ea89047cdfca850ea025fa36c6fc87.data",
+    "Build/9305710dd527d5246870686492dd49e8.wasm",
     "TemplateData/style.css"
 
 ];

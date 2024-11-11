33a1c698c3e6857e3cb237367e60c9167f2c619c
LAPTOP-3QO177P3\shahp <shahprenik@gmail.com>
2024-11-11 11:43:53 +0530

    Update CTA from psgaming to google

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "psgaming-PirateRush_Cocacola-0.1";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/08be164d17ff39ab3c365775127f0ed1.framework.js",
-    "Build/9231715d7b69007807068ef726c76c90.data",
+    "Build/f9e8675217436ab253fe4413a74c9e89.data",
     "Build/9305710dd527d5246870686492dd49e8.wasm",
     "TemplateData/style.css"
 
